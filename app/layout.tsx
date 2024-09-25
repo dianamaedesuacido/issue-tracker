8388d5640b650c1a5613008e1c81a11c97251892
Diana Mae <dianamae@example.com>
2024-09-25 14:39:41 +0800

    Show the issues

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,7 +24,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.variable}>
-        <Theme appearance="light" accentColor="violet">
+        <Theme
+          // appearance="light"
+          accentColor="violet"
+        >
           <NavBar />
           <main className="p-5">{children}</main>
         </Theme>
