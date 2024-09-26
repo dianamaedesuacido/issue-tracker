9bbe1f871674b51540286d29d66cdf61d1284494
Diana Mae <dianamae@example.com>
2024-09-26 15:51:44 +0800

    Disable SSR for loading md editor

--- app/issues/new/loading.tsx
+++ app/issues/new/loading.tsx
@@ -1,4 +1,4 @@
-import { Box, Flex, Card } from "@radix-ui/themes";
+import { Box } from "@radix-ui/themes";
 import React from "react";
 import Skeleton from "react-loading-skeleton";
 import "react-loading-skeleton/dist/skeleton.css";
--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { Button, Callout, Text, TextField } from "@radix-ui/themes";
+// import dynamic from "next/dynamic";
 import { useForm, Controller } from "react-hook-form";
 import SimpleMDE from "react-simplemde-editor";
 import "easymde/dist/easymde.min.css";
@@ -12,6 +13,10 @@ import { createIssueSchema } from "@/app/validationSchemas";
 import ErrorMessage from "@/app/components/ErrorMessage";
 import Spinner from "@/app/components/Spinner";
 
+// const SimpleMDE = dynamic(() =>
+//   import('react-simplemde-editor'),
+//   { ssr: false}
+// )
 type IssueForm = z.infer<typeof createIssueSchema>;
 
 const NewIssuePage = () => {
