575bbd7693975c31660f68a285fdd36022b6c433
Diana Mae <dianamae@example.com>
2024-09-26 16:18:39 +0800

    Refactor: Organize import statement

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,17 +1,16 @@
 "use client";
-import { Button, Callout, Text, TextField } from "@radix-ui/themes";
+import { Button, Callout, TextField } from "@radix-ui/themes";
 // import dynamic from "next/dynamic";
-import { useForm, Controller } from "react-hook-form";
-import SimpleMDE from "react-simplemde-editor";
-import "easymde/dist/easymde.min.css";
+import { ErrorMessage, Spinner } from "@/app/components";
+import { createIssueSchema } from "@/app/validationSchemas";
+import { zodResolver } from "@hookform/resolvers/zod";
 import axios from "axios";
+import "easymde/dist/easymde.min.css";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
+import { Controller, useForm } from "react-hook-form";
+import SimpleMDE from "react-simplemde-editor";
 import { z } from "zod";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { createIssueSchema } from "@/app/validationSchemas";
-import ErrorMessage from "@/app/components/ErrorMessage";
-import Spinner from "@/app/components/Spinner";
 
 // const SimpleMDE = dynamic(() =>
 //   import('react-simplemde-editor'),
--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,10 +1,9 @@
-import React from "react";
-import { Table } from "@radix-ui/themes";
+import { Link } from "@/app/components";
 import prisma from "@/prisma/client";
+import { Table } from "@radix-ui/themes";
+import delay from "delay";
 import IssueStatusBadge from "../components/IssueStatusBadge";
 import IssueActions from "./IssueActions";
-import delay from "delay";
-import Link from "@/app/components/Link";
 
 const IssuesPage = async () => {
   const issues = await prisma.issue.findMany();
