cadcb4957a205c515d53c52c5158d736f6dbfa71
Diana Mae <dianamae@example.com>
2024-09-26 15:04:33 +0800

    Build a styled link component

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -4,7 +4,7 @@ import prisma from "@/prisma/client";
 import IssueStatusBadge from "../components/IssueStatusBadge";
 import IssueActions from "./IssueActions";
 import delay from "delay";
-import Link from "next/link";
+import Link from "@/app/components/Link";
 
 const IssuesPage = async () => {
   const issues = await prisma.issue.findMany();
