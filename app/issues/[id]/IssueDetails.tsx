52232fda532d62a534f3162d5513282401d30c68
Diana Mae <dianamae@example.com>
2024-10-01 17:13:57 +0800

    Generating dummy data

--- app/issues/[id]/IssueDetails.tsx
+++ app/issues/[id]/IssueDetails.tsx
@@ -1,5 +1,6 @@
 import { IssueStatusBadge } from "@/app/components";
 import { Issue } from "@prisma/client";
+import React from "react";
 import { Card, Flex, Heading, Text } from "@radix-ui/themes";
 import ReactMarkdown from "react-markdown";
 
---
+++ prisma/migrations/20240930102351_change_datetimes_for_the_meantime/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE `issue` ALTER COLUMN `createdAt` DROP DEFAULT;
